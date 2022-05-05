2ef699f31a4d515aa0955b86dad3be29da611f38
Artem <kalaficzkij@ukr.net>
2022-05-05 19:40:00 +0300

    last version of prj, develop "language film"(start)

--- cms/static/cms/scripts/imgload.js
+++ cms/static/cms/scripts/imgload.js
@@ -8,11 +8,5 @@ $('#delete-picture').click(function () {
 })
 
 
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
 
 
