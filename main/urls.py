d3e77f68aaccb9ebb9b85b0e189949368524fe76
Artem <kalaficzkij@ukr.net>
2022-06-07 01:04:59 +0300

    last version of prj, another main pages(promotions)

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import main, poster, soon, cinemas, DetailCinemaView
+from .views import main, poster, soon, cinemas, DetailCinemaView, DetailHallView, promotions
 
 
 urlpatterns = [
@@ -8,6 +8,9 @@ urlpatterns = [
     path('poster/', poster, name='poster'),
     path('soon/', soon, name='soon'),
     # cinemas
-    path('cinemas/<int:pk>/cinema', DetailCinemaView.as_view(), name='concrete_cinema'),
+    path('cinemas/cinema/<int:pk>/', DetailCinemaView.as_view(), name='concrete_cinema'),
+    path('cinemas/cinema/hall/<int:pk>', DetailHallView.as_view(), name='concrete_hall'),
     path('cinemas/', cinemas, name='cinemas'),
+    # promotions
+    path('promotions/', promotions, name='promotions'),
 ]
--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from django.views.generic import DetailView
 
-from .models import Page, Banner, Slide, Carousel, Film, Cinema, Hall, Session
+from .models import Page, Banner, Slide, Carousel, Film, Cinema, Hall, Session, NewsOrProm
 
 
 def main(request):
@@ -20,6 +20,7 @@ def main(request):
     return render(request, 'main/pages/index.html', context)
 
 
+# region POSTER/SOON
 def poster(request):
     context = {'current_films': Film.objects.filter(premier_date__lte=datetime.date.today()),
                'soon_films': Film.objects.filter(premier_date__gt=datetime.date.today()),
@@ -32,8 +33,10 @@ def soon(request):
                'soon_films': Film.objects.filter(premier_date__gt=datetime.date.today()),
                'main_page': Page.objects.get(is_main=True)}
     return render(request, 'main/pages/soon.html', context)
+# endregion POSTER/SOON
 
 
+# region CINEMAS
 def cinemas(request):
     context = {'cinemas': Cinema.objects.all(),
                'main_page': Page.objects.get(is_main=True)}
@@ -47,7 +50,7 @@ class DetailCinemaView(DetailView):
     def get_context_data(self, **kwargs):
         context = super(DetailCinemaView, self).get_context_data(**kwargs)
         context['main_page'] = Page.objects.get(is_main=True)
-        context['images'] = get_object_or_404(Cinema, pk=self.kwargs['pk']).images.all()
+        context['images'] = self.object.images.all()
         halls = Hall.objects.filter(cinema=self.kwargs['pk'])
         idx = []
         for hall in halls:
@@ -56,3 +59,24 @@ class DetailCinemaView(DetailView):
         context['halls'] = halls
         context['count_halls'] = len(halls)
         return context
+
+
+class DetailHallView(DetailView):
+    model = Hall
+    template_name = 'main/pages/cinemas/hall.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(DetailHallView, self).get_context_data(**kwargs)
+        context['main_page'] = Page.objects.get(is_main=True)
+        context['sessions'] = Session.objects.filter(hall=self.kwargs['pk'])
+        context['images'] = self.object.images.all()
+        return context
+# endregion CINEMAS
+
+
+# region PROMOTIONS
+def promotions(request):
+    context = {'main_page': Page.objects.get(is_main=True),
+               'promotions': NewsOrProm.objects.filter(type=False)}
+    return render(request, 'main/pages/promotions/promotions.html', context)
+# endregion PROMOTIONS
